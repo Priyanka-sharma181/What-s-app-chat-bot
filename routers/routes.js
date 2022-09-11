18f5a292a8958c7b0380a52ad324fc5ae5f6381b
Priyanka-sharma181 <Priyanka-sharma181>
2022-09-11 19:46:20 +0530

    routes

--- routers/routes.js
+++ routers/routes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { sendMessage, webhookForText } = require("../controller/chatbot")
+const { sendMessage } = require("../controller/chatbot")
 const router = express.Router()
 
 router.post("/sendMessage",sendMessage)
